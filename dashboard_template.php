a832535e5216c6130414368e1eb884281ccf5929
irivero <45241620+irivero1@users.noreply.github.com>
2020-07-15 18:00:52 -0500

    Eliminados archivos no utilizados/residuos

--- dashboard_template.php
+++ dashboard_template.php
@@ -4,7 +4,7 @@
 <!doctype html>
 <html>
 <head>
-	<title>#YoCuidoDeTi - asdasdasd Dashboard</title>
+	<title>#YoCuidoDeTi - PRUEBA Dashboard</title>
 	<meta charset="utf-8">
 
 	<link rel="stylesheet" href="css/bootstrap.min.css">
@@ -22,17 +22,17 @@
 		  <a id="logo" class="navbar-brand ml-5" href="index.html">
 		    #YoCuidoDeTi
 		  </a>
-		  <p class="navbar-brand pr-5 mb-0 ">Panel de adasdsadasd</p>
+		  <p class="navbar-brand pr-5 mb-0 ">Panel de PRUEBA</p>
 		  <button class="btn btn-outline-light mr-5"><i class="fas fa-sign-out-alt"></i><a href="php/salir.php" style="color:inherit;"> Salir</a></button> 
 		</nav>
 	</div>
 	<div class="container-full all-container">
 		<div class="d-flex row p-5" id="wrap-contenido">
 			<div class="col-10 mx-auto py-3 px-4">
-				<h2 class="text-center">Contenido para ddasdasd</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis. Mauris in aliquam sem fringilla ut morbi tincidunt. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Hac habitasse platea dictumst vestibulum. In fermentum posuere urna nec tincidunt praesent semper feugiat. Mattis pellentesque id nibh tortor id aliquet. Id diam maecenas ultricies mi eget. Integer vitae justo eget magna fermentum. Quis commodo odio aenean sed adipiscing diam. Dolor sed viverra ipsum nunc aliquet bibendum enim. <br><br>
+			
+			<!-- CONTENIDO AQUI -->
+					
 
-				Eget nunc scelerisque viverra mauris in aliquam sem fringilla. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat. Ultrices mi tempus imperdiet nulla. Dui id ornare arcu odio ut sem nulla pharetra diam. Massa id neque aliquam vestibulum morbi blandit. Dui nunc mattis enim ut tellus elementum sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada. Felis imperdiet proin fermentum leo vel orci porta non. Praesent tristique magna sit amet purus gravida quis blandit turpis. Quisque sagittis purus sit amet volutpat consequat mauris nunc congue. Mauris pharetra et ultrices neque ornare aenean. Posuere urna nec tincidunt praesent semper feugiat nibh. Consequat semper viverra nam libero justo. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Et odio pellentesque diam volutpat commodo sed..</p>
 			</div>
 		</div>
 	</div>
